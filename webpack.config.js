7b7af9616e5e01fd772826d207b65ee931905700
F-P-S-B <59065604+F-P-S-B@users.noreply.github.com>
2021-07-05 22:30:48 +0200

    Created basic files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new BundleAnalyzerPlugin()],
+
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    watchContentBase: true,
+    contentBasePublicPath: "/",
+    // disableHostCheck: true,
+    port: 9000,
+  },
+  mode: "development",
+};
